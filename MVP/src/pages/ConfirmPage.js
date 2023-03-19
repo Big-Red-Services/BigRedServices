b0e9aae9d1d5bb58f4bf2a420dc6eb08c2d85d4d
Yanjun Zhao <yanjunzhao81@gmail.com>
2023-03-19 13:09:27 -0400

    finished initial confirmation page

--- MVP/src/pages/ConfirmPage.js
+++ MVP/src/pages/ConfirmPage.js
@@ -1,9 +1,17 @@
 export default function ConfirmPage() {
+    const handleClick = (e) => {
+        window.location.href = "http://localhost:3000";
+    }
+
     return (
-        <div className="progressPage">
-            <h1>⚠️Work In Progress⚠️</h1>
-            <h2>this is going to be the confirmation page</h2>
+        <div className="confirmationPage">
+            <body>
+            <button className= "homeButton" style={{height:'70px',width:'140px', borderRadius: '10px', font: 100}} onClick={(e) => handleClick(e)}>Home</button>
+            <h1>Thank You for applying for this job!</h1>
+            <h2>You will receive an update soon from the Job Poster!</h2>
+            </body>
         </div>
+
     );
 
 }
